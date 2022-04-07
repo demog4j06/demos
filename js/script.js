d5c3d4b422f8dfe7a1f3b45b0eba5dda60610e1a
demog4j06 <102815642+demog4j06@users.noreply.github.com>
2022-04-07 19:03:59 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             $('.demo-list').append(item);
             $(item).click(function(){
                 var gamenameurl = data.demos[$(this).index()].gameName.replace(/ |'/g,"").toLowerCase();
-                $('.demo-block').show().html('<div class="close">྾</div><iframe src="https://play.8provider.com/game/fullstate/html5/evoplay/' + gamenameurl + '/?project=5725&user_id=30657361&demo=0&wid=1&s=1cda4af0d48d52628d443e3c268a3ba0"/>');
+                $('.demo-block').show().html('<div class="close">྾</div><iframe src="https://play.8provider.com/game/fullstate/html5/evoplay/' + gamenameurl + '/?project=5725&user_id=30661313&demo=0&wid=1&s=752b016a5bb3d5b33bee9e2000b5580e"/>');
                 $('.demo-block .close').click(function(){
                     $('.demo-block').hide().html('');
                 });
