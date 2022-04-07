83e37a2eb4d37b0b585f181b84f17b1809994055
demog4j06 <102815642+demog4j06@users.noreply.github.com>
2022-04-07 21:18:27 +0800

    test

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
 $(document).ready(function(){
-    $.getJSON("demos.json", function(data){
-        for (var i = 0; i < data.demos.length; i++) {
-            var item = $('<div class="game"><img src="./img/covers/' 
-                + data.demos[i].gameID + 
-                '.png"><h3>' 
-                + data.demos[i].gameName + 
-                '</h3></div>');
-            $('.demo-list').append(item);
-            $(item).click(function(){
-                var gamenameurl = data.demos[$(this).index()].gameName.replace(/ |'/g,"").toLowerCase();
-                $('.demo-block').show().html('<div class="close">྾</div><iframe src="https://play.8provider.com/game/fullstate/html5/evoplay/' + gamenameurl + '?project=5725&user_id=6843706&demo=1&wid=1&s=03beab669abe14f64d0171a60a154213"/>');
-                $('.demo-block .close').click(function(){
-                    $('.demo-block').hide().html('');
-                });
-            });
-        }
-    }).fail(function(){
-        console.log("ERROR");
-    });
+    $( ".demo-list" ).load( "https://gleaming-torte-2c8957.netlify.app/demo.html" );
 });
